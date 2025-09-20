bd164f39e24fcde2e5a57a6262c2cbaeab6c8f2c
Akbar-Widya <akbarwidya@student.telkomuniversity.ac.id>
2025-09-20 17:48:33 +0700

    Implement Routing, Dashboard Layout & UI, Auth Functionality

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, route, layout } from "@react-router/dev/routes"; // Adjust import based on your setup
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout("routes/admin/admin-layout.tsx", [
+    route("dashboard", "./routes/admin/dashboard.tsx"), // Correct: path, then file path
+    route("all-users", "./routes/admin/all-users.tsx"), // Correct: path, then file path
+  ]),
+] satisfies RouteConfig;
